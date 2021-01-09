ed95eff7bb2312f64f25fd1f8b4450a6c970d33a
cnnrmnn <connor.mann@outlook.com>
2021-01-09 11:31:50 -0500

    Fixed typo

--- src/api/middleware/validateWith.js
+++ src/api/middleware/validateWith.js
@@ -2,7 +2,7 @@ export default function (schema) {
     return (req, res, next) => {
         const { error } = schema.validate(req.body);
         if (error)
-            return res.status(400),send({
+            return res.status(400).send({
                 error: error.details[0].message
             });
         next();

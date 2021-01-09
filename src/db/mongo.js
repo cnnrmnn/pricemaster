30a9471f3245969d81266a5e6ed4b1eec746d7b5
cnnrmnn <connor.mann@outlook.com>
2021-01-09 11:47:47 -0500

    Fixed a default option

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export default async () =>
     mongoose.connect(process.env.DB_CONNECTION_STRING, {
         useCreateIndex: true,
-        useFindAndModify: true,
+        useFindAndModify: false,
         useNewUrlParser: true,
         useUnifiedTopology: true
     });

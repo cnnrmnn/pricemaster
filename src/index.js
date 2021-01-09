e73d97f47a377373ec63ad675529a0987e7fe0ff
cnnrmnn <connor.mann@outlook.com>
2021-01-09 11:19:36 -0500

    Added punctuation to log messages

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import express from "express";
 import helmet from "helmet";
 import connectMongo from "./db/mongo";
 import router from "./api/router";
+
 const app = express();
 
 async function start() {
     console.log("Connecting to database...");
     await connectMongo();
-    console.log("Connected to database");
+    console.log("Connected to database.");
      
     app.use(express.json());
     app.use(helmet());
@@ -15,7 +16,7 @@ async function start() {
     router(app);
 
     const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
 }
 
 start();

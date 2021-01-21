a12c7f26233877f8371c146ca616150beb1924f1
cnnrmnn <connor.mann@outlook.com>
2021-01-21 11:32:25 -0500

    Changed parameter name

--- src/services/event.js
+++ src/services/event.js
@@ -1,17 +1,17 @@
 import { Event } from '../models/event';
 import { agenda } from '../jobs/jobs';
 
-export async function upsertEvent(id, body) {
-    const event = await Event.findByIdAndUpdate(id, body, {
+export async function upsertEvent(eventId, body) {
+    const event = await Event.findByIdAndUpdate(eventId, body, {
         upsert: true,
         new: true,
     });
     if (!event) {
-        console.error(`Failed to upsert event ${id}.`);
+        console.error(`Failed to upsert event ${eventId}.`);
         return;
     }
     if (!event.jobId) {
-        const job = agenda.create('updateCheapestTicket');
+        const job = agenda.create('updateCheapestTicket', { eventId });
         job.repeatEvery(process.env.UPDATE_INTERVAL, { endDate: event.date });
         await job.save();
         event.jobId = job.attrs._id;

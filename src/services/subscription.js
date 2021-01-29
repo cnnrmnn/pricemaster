a3f3f40b75a5be8037eff43919eb501a14831ef8
cnnrmnn <connor.mann@outlook.com>
2021-01-29 15:03:03 -0500

    Started catching errors in subscription service

--- src/services/subscription.js
+++ src/services/subscription.js
@@ -3,25 +3,50 @@ import { Subscription } from '../models/subscription';
 import { updateEventInfo } from './ticketmaster';
 
 export async function createSubscription(body) {
-    const subscription = await Subscription.create(body);
-    await updateEventInfo(subscription.eventId, subscription._id);
+    let subscription;
+    try {
+        subscription = await Subscription.create(body);
+        await updateEventInfo(subscription.eventId, subscription._id);
+    } catch (err) {
+        console.error(`Failed to create subscription`);
+        console.error(err);
+    }
     return subscription;
 }
 
 export async function updateSubscription(subscriptionId, body) {
-    const subscription = await Subscription.findByIdAndUpdate(
-        subscriptionId,
-        body,
-        { new: true }
-    );
+    let subscription;
+    try {
+        subscription = await Subscription.findByIdAndUpdate(
+            subscriptionId,
+            body,
+            { new: true }
+        );
+    } catch (err) {
+        console.error(`Failed to update subscription ${subscriptionId}`);
+        console.error(err);
+    }
     return subscription;
 }
 
 export async function deleteSubscription(subscriptionId) {
-    const subscription = await Subscription.findByIdAndDelete(subscriptionId);
+    let subscription;
+    try {
+        subscription = await Subscription.findByIdAndDelete(subscriptionId);
+    } catch (err) {
+        console.error(`Failed to delete subscription ${subscriptionId}`);
+        console.error(err);
+    }
     if (!subscription) return;
-    await Event.findByIdAndUpdate(subscription.eventId, {
-        $pull: { subscriptionIds: subscriptionId },
-    });
+    try {
+        await Event.findByIdAndUpdate(subscription.eventId, {
+            $pull: { subscriptionIds: subscriptionId },
+        });
+    } catch (err) {
+        console.error(
+            `Failed to remove subscription ${subscriptionId} from event ${subscription.eventId}`
+        );
+        console.error(err);
+    }
     return subscription;
 }

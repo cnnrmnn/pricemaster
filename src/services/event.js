2dff377b7321f074e5fd42f37755320edd1767d8
cnnrmnn <connor.mann@outlook.com>
2021-01-29 14:38:16 -0500

    Started catching errors in event service

--- src/services/event.js
+++ src/services/event.js
@@ -2,20 +2,43 @@ import { Event } from '../models/event';
 import { agenda } from '../jobs/jobs';
 
 export async function upsertEvent(eventId, body) {
-    const event = await Event.findByIdAndUpdate(eventId, body, {
-        upsert: true,
-        new: true,
-    });
+    try {
+        const event = await Event.findByIdAndUpdate(eventId, body, {
+            upsert: true,
+            new: true,
+        });
+    } catch (err) {
+        console.error(`Failed to upsert event ${eventId}.`);
+        console.error(err);
+    }
+
     if (!event) {
         console.error(`Failed to upsert event ${eventId}.`);
         return;
     }
+
     if (!event.jobId) {
-        const job = agenda.create('updateCheapestTicket', { eventId });
-        job.repeatEvery(process.env.UPDATE_INTERVAL, { endDate: event.date });
-        await job.save();
-        event.jobId = job.attrs._id;
-        await event.save();
+        try {
+            const job = agenda.create('updateCheapestTicket', { eventId });
+            job.repeatEvery(process.env.UPDATE_INTERVAL, {
+                endDate: event.date,
+            });
+            await job.save();
+        } catch (err) {
+            console.error(
+                `Failed to create updateCheapestTicket job for event ${eventId}`
+            );
+        }
+
+        try {
+            event.jobId = job.attrs._id;
+            await event.save();
+        } catch (err) {
+            console.error(
+                `Failed to save job ${job.attrs._id} to event ${eventId}`
+            );
+            console.error(err);
+        }
     }
     return event;
 }

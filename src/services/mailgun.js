fcb42199045e1e886dcfef2f4980c846cd270576
cnnrmnn <connor.mann@outlook.com>
2021-01-29 13:42:33 -0500

    Started catching errors in mailgun service

--- src/services/mailgun.js
+++ src/services/mailgun.js
@@ -2,12 +2,17 @@ import axios from 'axios';
 
 export async function sendEmail(to, subject, text) {
     const endpoint = `${process.env.MAILGUN_BASE_URL}/${process.env.MAILGUN_DOMAIN}/messages`;
-    await axios.post(endpoint, {
-        data: {
-            from: process.env.MAILGUN_EMAIL,
-            to,
-            subject,
-            text,
-        },
-    });
+    try {
+        await axios.post(endpoint, {
+            data: {
+                from: process.env.MAILGUN_EMAIL,
+                to,
+                subject,
+                text,
+            },
+        });
+    } catch (err) {
+        console.error(`Failed to send email ${subject} to ${to}`);
+        console.error(err);
+    }
 }
